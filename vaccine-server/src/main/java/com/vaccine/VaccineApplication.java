557b2a54e9d88fb4dc82f129347199ae8167336b
qiusen <2559231099@qq.com>
2023-12-06 21:49:49 +0800

    [add] mybatis-plus-generate maven addtional

--- vaccine-server/src/main/java/com/vaccine/VaccineApplication.java
+++ vaccine-server/src/main/java/com/vaccine/VaccineApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
--- vaccine-server/src/main/java/com/vaccine/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.vaccine.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.vaccine.entity.User;
-import org.springframework.stereotype.Service;
-
-
-public interface UserService extends IService<User> {
-}
